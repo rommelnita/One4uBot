398b7dc8ac4621e192b272bf72125901e174f5a6
wonantes <86704064+wonantes@users.noreply.github.com>
2021-11-12 07:27:59 +0700

    Update spam.py

--- userbot/modules/spam.py
+++ userbot/modules/spam.py
@@ -8,86 +8,50 @@ from asyncio import wait, sleep
 
 from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP
 from userbot.events import register
+from telethon.sync import TelegramClient
+from telethon.tl.functions.messages import GetDialogsRequest
+from telethon.tl.types import InputPeerEmpty, InputPeerChannel, InputPeerUser
+import sys
+import traceback
+import time
+import random
+import re
 
-
-@register(outgoing=True, pattern="^.cspam (.*)")
-async def tmeme(e):
-    cspam = str(e.pattern_match.group(1))
-    message = cspam.replace(" ", "")
-    await e.delete()
-    for letter in message:
-        await e.respond(letter)
-    if BOTLOG:
-        await e.client.send_message(
-            BOTLOG_CHATID, "#CSPAM\n"
-            "TSpam was executed successfully")
-
-
-@register(outgoing=True, pattern="^.wspam (.*)")
-async def tmeme(e):
-    wspam = str(e.pattern_match.group(1))
-    message = wspam.split()
-    await e.delete()
-    for word in message:
-        await e.respond(word)
-    if BOTLOG:
-        await e.client.send_message(
-            BOTLOG_CHATID, "#WSPAM\n"
-            "WSpam was executed successfully")
-
-
-@register(outgoing=True, pattern="^.spam (.*)")
-async def spammer(e):
-    counter = int(e.pattern_match.group(1).split(' ', 1)[0])
-    spam_message = str(e.pattern_match.group(1).split(' ', 1)[1])
-    await e.delete()
-    await asyncio.wait([e.respond(spam_message) for i in range(counter)])
-    if BOTLOG:
-        await e.client.send_message(BOTLOG_CHATID, "#SPAM\n"
-                                    "Spam was executed successfully")
-
-
-@register(outgoing=True, pattern="^.picspam")
-async def tiny_pic_spam(e):
-    message = e.text
-    text = message.split()
-    counter = int(text[1])
-    link = str(text[2])
-    await e.delete()
-    for i in range(1, counter):
-        await e.client.send_file(e.chat_id, link)
-    if BOTLOG:
-        await e.client.send_message(
-            BOTLOG_CHATID, "#PICSPAM\n"
-            "PicSpam was executed successfully")
-
-
-@register(outgoing=True, pattern="^.delayspam (.*)")
-async def spammer(e):
-    spamDelay = float(e.pattern_match.group(1).split(' ', 2)[0])
-    counter = int(e.pattern_match.group(1).split(' ', 2)[1])
-    spam_message = str(e.pattern_match.group(1).split(' ', 2)[2])
-    await e.delete()
-    for i in range(1, counter):
-        await e.respond(spam_message)
-        await sleep(spamDelay)
-    if BOTLOG:
-        await e.client.send_message(
-            BOTLOG_CHATID, "#DelaySPAM\n"
-            "DelaySpam was executed successfully")
-
-
-CMD_HELP.update({
-    "spam":
-    ".cspam <text>\
-\nUsage: Spam the text letter by letter.\
-\n\n.spam <count> <text>\
-\nUsage: Floods text in the chat !!\
-\n\n.wspam <text>\
-\nUsage: Spam the text word by word.\
-\n\n.picspam <count> <link to image/gif>\
-\nUsage: As if text spam was not enough !!\
-\n\n.delayspam <delay> <count> <text>\
-\nUsage: .bigspam but with custom delay.\
-\n\n\nNOTE : Spam at your own risk !!"
-})
+api_id = 8432622  # API ID
+api_hash = '1911d40c63e90803fc62d64f1678b9a3'  #API HASH
+phone = '+6282226037375'  #NUMBER WITH COUNTRY CODE
+timer = 120 #TIME TO WAIT BEFORE NEXT SENDING
+msgtosend = "Metpagi" # MESSAGE TO SEND
+client = TelegramClient(phone, api_id, api_hash)
+client.connect()
+if not client.is_user_authorized():
+    client.send_code_request(phone)
+    client.sign_in(phone, input('Enter the code: '))
+chats = []
+last_date = None
+chunk_size = 10
+groups=[]
+result = client(GetDialogsRequest(
+                offset_date=last_date,
+                offset_id=0,
+                offset_peer=InputPeerEmpty(),
+                limit=chunk_size,
+                hash = 0
+            ))
+chats.extend(result.chats)
+for chat in chats:
+    try:
+        groups.append(chat)
+    except:
+        continue
+while True:    
+    for group in groups:
+        try:
+            username1 = group.username
+            print(username1)    
+            client.send_message(username1, msgtosend)
+            time.sleep(1)
+        except:
+            continue 
+    print("Send Complete!!, Waiting for " + str(timer) + " seconds")
+    time.sleep(timer)
